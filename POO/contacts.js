6ffc32fd5f94572af4e29c9efe7bf79a6ed5f356
EsterSJ <ester_s@hotmail.com>
2023-03-15 11:10:09 +0100

    modificacion clase contacts

--- POO/contacts.js
+++ POO/contacts.js
@@ -1,12 +1,12 @@
-const myClass = require("./person");
-
-var persona1 = new myClass.Person("Ester", "Sanchez", "castaño", false, "Navalcarnero", "femenino", 58, 1.68, 1985, ["comer", "leer", "patinar"]);
-var persona2 = new myClass.Person("Jorge", "Martin", "rubio", true, "Madrid", "masculino", 75, 1.89, 2001, ["cine", "ropa", "jugar a la play"]);
-var persona3 = new myClass.Person("Paula", "Gonzalo", "moreno", true, "Salamanca", "femenino", 63, 1.59, 1990, ["salir", "cantar", "cocinar"]);
-
 class Contacts {
     constructor (){
-        this.listapersonas = [persona1, persona2, persona3];
+        this.listapersonas = [];
+    }
+
+    rellenarContactos (c1, c2, c3){
+        this.listapersonas.push(c1);
+        this.listapersonas.push(c2);
+        this.listapersonas.push(c3);
     }
 
     printPersons (){
--- POO/contactsTest.js
+++ POO/contactsTest.js
@@ -1,4 +1,10 @@
-const myClass = require("./contacts");
+const myClassContacts = require("./contacts");
+const myClassPerson = require("./person");
 
-let listaContactos = new myClass.Contacts();
+var persona1 = new myClassPerson.Person("Ester", "Sanchez", "castaño", false, "Navalcarnero", "femenino", 58, 1.68, 1985, ["comer", "leer", "patinar"]);
+var persona2 = new myClassPerson.Person("Jorge", "Martin", "rubio", true, "Madrid", "masculino", 75, 1.89, 2001, ["cine", "ropa", "jugar a la play"]);
+var persona3 = new myClassPerson.Person("Paula", "Gonzalo", "moreno", true, "Salamanca", "femenino", 63, 1.59, 1990, ["salir", "cantar", "cocinar"]);
+
+let listaContactos = new myClassContacts.Contacts();
+listaContactos.rellenarContactos(persona1, persona2, persona3);
 listaContactos.printPersons();
